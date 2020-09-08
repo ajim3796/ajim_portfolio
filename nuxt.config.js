a37c8ddc84437536af4fcf99732a3de088feac0b
ajim <n1a1o2k0i@gmail.com>
2020-09-08 17:44:17 +0900

    client

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
+const contentful = require("contentful");
+const client = contentful.createClient({
+  space: process.env.CTF_SPACE_ID,
+  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+});
+
 export default {
   /*
    ** Nuxt rendering mode
